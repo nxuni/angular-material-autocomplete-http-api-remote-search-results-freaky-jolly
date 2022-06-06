6482849fda8ebceb82db096828cb5603a9dbabda
Carlos A Benitez M <ca.benitez.m@msn.com>
2022-06-06 02:54:49 -0500

    Adding Server-Side Mat Autocomplete Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+import { debounceTime, tap, switchMap, finalize, distinctUntilChanged, filter } from 'rxjs/operators';
+
+const API_KEY = "e8067b53"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'new-ng-app';
+export class AppComponent implements OnInit {
+
+  searchMoviesCtrl = new FormControl();
+  filteredMovies: any;
+  isLoading = false;
+  errorMsg!: string;
+  minLengthTerm = 3;
+  selectedMovie: any = "";
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  onSelected() {
+    console.log(this.selectedMovie);
+    this.selectedMovie = this.selectedMovie;
+  }
+
+  displayWith(value: any) {
+    return value?.Title;
+  }
+
+  clearSelection() {
+    this.selectedMovie = "";
+    this.filteredMovies = [];
+  }
+
+  ngOnInit() {
+    this.searchMoviesCtrl.valueChanges
+      .pipe(
+        filter(res => {
+          return res !== null && res.length >= this.minLengthTerm
+        }),
+        distinctUntilChanged(),
+        debounceTime(1000),
+        tap(() => {
+          this.errorMsg = "";
+          this.filteredMovies = [];
+          this.isLoading = true;
+        }),
+        switchMap(value => this.http.get('http://www.omdbapi.com/?apikey=' + API_KEY + '&s=' + value)
+          .pipe(
+            finalize(() => {
+              this.isLoading = false
+            }),
+          )
+        )
+      )
+      .subscribe((data: any) => {
+        if (data['Search'] == undefined) {
+          this.errorMsg = data['Error'];
+          this.filteredMovies = [];
+        } else {
+          this.errorMsg = "";
+          this.filteredMovies = data['Search'];
+        }
+        console.log(this.filteredMovies);
+      });
+  }
 }
